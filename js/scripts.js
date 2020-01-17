a5fb767e945e1f5da40b112051046f6dfbca3de4
Student <example@example.com>
2020-01-17 11:52:20 -0800

    add user interface logic in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
+    var color = parseInt($("#color").val());
+    var browser = parseInt($("#browser").val());
+    var pc = parseInt($("#pc").val());
+    var city = parseInt($("#city").val());
+    var lego = parseInt($("#city").val());
+    var result = (color + browser + pc + city + lego);
 
-  });
+  if (result <= 7) {
+    $("#csharp").show();
+    $("#python, #ruby").hide();
+
+  } else if (result <= 14) {
+      $("#python").show();
+      $("#csharp, #ruby").hide();
+
+  } else if (result <= 22) {
+      $("#ruby").show();
+      $("#csharp, #python").hide();
+
+      return (result);
+
+  };
+
+});
 });
